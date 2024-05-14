73b578b1c74110d855e112e81a8d7dd0de70bae0
Dushan Chanuka <chanukadushan1030@gmail.com>
2024-05-14 23:22:12 +0530

    chore: Update vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dushans-portfolio/', // base URL for GitHub Pages
+  base: '/dushans-portfolio/', 
 });
 
